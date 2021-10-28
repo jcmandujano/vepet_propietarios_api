30ac36aa64fa27c80d54308cfc9a99bfbcfcf510
José Armando Ramírez Carrillo <ing.armandoramirez@outlook.com>
2021-10-27 21:14:59 -0500

    Se creo el CRUD de especies, con la coleccion que ya estaba enmongo

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,46 +1,59 @@
-const  Role  = require('../models/role')
+const Role = require('../models/role')
 const Usuario = require('../models/user.model');
-const Mascota = require('../models/pets.model')
+const Mascota = require('../models/pets.model');
+const Especie = require('../models/species.model');
 
-const esRolValido = async(rol='')=>{
-    const existeRol = await Role.findOne({rol});
-    if(!existeRol){
+const esRolValido = async(rol = '') => {
+    const existeRol = await Role.findOne({ rol });
+    if (!existeRol) {
         throw new Error(`El rol ${rol} no existe en BD`)
     }
-} 
+}
 
-const emailDisponible = async(email='')=>{
+const emailDisponible = async(email = '') => {
     //verificar si el correo existe
-    const existeEmail = await Usuario.findOne({correo:email})
-    if(existeEmail){
+    const existeEmail = await Usuario.findOne({ correo: email })
+    if (existeEmail) {
         throw new Error(`El email ya esta registrado`)
     }
 }
 
-const existeIdUsuario = async(id)=>{
+const existeIdUsuario = async(id) => {
     //verificar si el usuario existe
     const existeUsuario = await Usuario.findById(id)
-    if(!existeUsuario){
+    if (!existeUsuario) {
         throw new Error(`El id de usuario no existe`)
     }
 }
 
-const existeIdMascota = async(id)=>{
+const existeIdMascota = async(id) => {
     //verificar si existe la mascota y que no este eliminada
     const query = {
-        estado : true
+        estado: true
     }
 
     const existeMascota = await Mascota.findById(id).where(query)
-    if(!existeMascota){
+    if (!existeMascota) {
         throw new Error(`El id de esta mascota no existe`)
     }
 }
 
+const existeIdEspecie = async(id) => {
+    //verificar si existe la mascota y que no este eliminada
+    const query = {
+        estado: true
+    }
+
+    const existeEspecie = await Especie.findById(id).where(query)
+    if (!existeEspecie) {
+        throw new Error(`El id de esta especie no existe`)
+    }
+}
 
 module.exports = {
     esRolValido,
     emailDisponible,
     existeIdUsuario,
-    existeIdMascota
+    existeIdMascota,
+    existeIdEspecie
 }
