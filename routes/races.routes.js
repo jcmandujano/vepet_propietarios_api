1b5c1f10238935c1c02ac1e98348d77fbb06fa3d
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2022-03-22 21:50:22 -0600

    Validacion del jwt a razas y especies

--- routes/races.routes.js
+++ routes/races.routes.js
@@ -3,6 +3,7 @@ const { check } = require('express-validator')
 const { readRaces, findRace, updateRace, createRaces, deleteRace } = require('../controllers/races.controller')
 const { existeIdRaza } = require('../helpers/db-validators')
 const { validarCampos } = require('../middlewares/validar-campos')
+const { validateJWT } = require('../middlewares/jwt-validator')
 
 const router = Router()
 
@@ -11,12 +12,14 @@ router.get('/', readRaces);
 router.get('/:id', findRace);
 
 router.put('/:id', [
+    validateJWT,
     check('id', 'No es un id valido').isMongoId(),
     check('id').custom(existeIdRaza),
     validarCampos
 ], updateRace);
 
 router.post('/', [
+    validateJWT,
     check('raza', 'El nombre es obligatorio').not().isEmpty(),
     check('especie', 'El identificador de la raza es obligatorio').not().isEmpty(),
     validarCampos
--- routes/species.routes.js
+++ routes/species.routes.js
@@ -3,6 +3,7 @@ const { check } = require('express-validator')
 const { readSpecies, findSpecie, updateSpecie, createSpecies, deleteSpecie } = require('../controllers/species.controller')
 const { existeIdEspecie } = require('../helpers/db-validators')
 const { validarCampos } = require('../middlewares/validar-campos')
+const { validateJWT } = require('../middlewares/jwt-validator')
 
 const router = Router()
 
@@ -11,12 +12,14 @@ router.get('/', readSpecies)
 router.get('/:id', findSpecie)
 
 router.put('/:id', [
+    validateJWT,
     check('id', 'No es un id valido').isMongoId(),
     check('id').custom(existeIdEspecie),
     validarCampos
 ], updateSpecie)
 
 router.post('/', [
+    validateJWT,
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     validarCampos
 ], createSpecies)
